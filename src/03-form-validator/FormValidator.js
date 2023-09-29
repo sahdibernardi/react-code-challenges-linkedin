8b5d40a81180de1c5885f48d613503435f87ca33
sahdibernardi <sah.dibernardi@gmail.com>
2023-09-29 15:34:59 -0300

    feat: finishes third challenge

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,6 +4,35 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [displayedMessage, setDisplayedMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // fields are all fulfilled
+    if (!email || !password || !passwordConfirm) {
+      setDisplayedMessage('Some fields are empty, please fill them all')
+      return
+    }
+    // password is at least 8 characters
+    if (password.length < 8) {
+      setDisplayedMessage('Password is too short. It should contain at least 8 characters')
+      return
+    }
+    // password and passwordConfirm match
+    if (password !== passwordConfirm) {
+      setDisplayedMessage('Passwords do not match')
+      return
+    }
+    // email has exactly one @
+    if (email.split('@').length !== 2) {
+      setDisplayedMessage('Email is invalid')
+      return
+    }
+    setDisplayedMessage('Success! User Registered')
+    setEmail('')
+    setPassword('')
+    setPasswordConfirm('')
+  }
 
   return (
     <form>
@@ -12,18 +41,22 @@ export default function FormValidator () {
       <input
         type='text' name='email'
         onChange={e => setEmail(e.target.value)}
+        value={email}
       />
       <label htmlFor='password'>Password</label>
       <input
         type='password' name='password'
         onChange={e => setPassword(e.target.value)}
+        value={password}
       />
       <label htmlFor='password-confirm'>Confirm Password </label>
       <input
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
+        value={passwordConfirm}
       />
-      <input type='submit' value='Submit' />
+      <p>{displayedMessage}</p>
+      <input type='submit' value='Submit'  onClick={(e) => handleSubmit(e)}/>
     </form>
   )
 }
