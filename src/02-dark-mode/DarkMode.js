85fcd69a32a6cdd387c641aae2d280f988b5d3e6
sahdibernardi <sah.dibernardi@gmail.com>
2023-09-29 15:01:12 -0300

    feat: solves dark mode challenge

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function DarkMode () {
+  const [isDarkModeOn, setIsDarkModeOn] = useState(false);
+
+  const changeDarkMode = (boolean) => {
+    boolean ? setIsDarkModeOn(true) : setIsDarkModeOn(false);
+    return;
+  }
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={isDarkModeOn ? 'page dark-mode' : 'page'}>
+      <button className='dark-mode-button' onClick={() => changeDarkMode(true)}>
+        Dark Mode
+      </button>
+      <button className='light-mode-button' onClick={() => changeDarkMode(false)}>
+        Light Mode
+      </button>
     </div>
   )
 }
