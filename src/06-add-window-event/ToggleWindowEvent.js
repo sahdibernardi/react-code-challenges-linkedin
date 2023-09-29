3ade28506681d4b639c2ebba1c99c9edebcbf172
sahdibernardi <sah.dibernardi@gmail.com>
2023-09-29 16:28:49 -0300

    solves challenge 6

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import WindowEvent from './WindowEvent'
+import { dblClick } from '@testing-library/user-event/dist/click';
 
 export default function ToggleWindowEvent () {
   const [windowEvent, setWindowEvent] = useState(false)
+
+  useEffect(() => {
+    if(windowEvent) {
+      const btnElement = document.getElementById('toggle-btn')
+      btnElement.addEventListener('dblclick', alert('Window Event is Active!'))
+    }
+  }, [windowEvent])
+
+
   return (
     <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
+      <button id='toggle-btn' onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
       {windowEvent && <WindowEvent />}
     </div>
   )
