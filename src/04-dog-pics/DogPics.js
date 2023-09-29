996239701b5cdc8b35655bca475d79c6641d554a
sahdibernardi <sah.dibernardi@gmail.com>
2023-09-29 15:51:49 -0300

    feat: integrating with api that searches for dogs

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,21 @@
+import { useState } from "react"
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+  const [dogPic, setDogPic] = useState('https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg')
+
+  const getDogPic = () => {
+    fetch ('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setDogPic(data.message))
+  }
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <h2>Sad today? No worries. We have dog pictures to cheer you up!</h2>
+      <p>Press the button below to see a new dog</p>
+      <img src={dogPic} alt='really cute dog'/>
+      <button onClick={getDogPic}>🐶</button>
     </div>
   )
 }
